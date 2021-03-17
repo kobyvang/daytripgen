a5c2f852de2833253f7b97a29bb8f6a9f5e0eb7d
kobyvang <80074281+kobyvang@users.noreply.github.com>
2021-03-16 21:00:10 -0500

    Add files via upload

--- trial.js
+++ trial.js
@@ -8,11 +8,18 @@ console.log("random destination, resturant, transport, and entertainment");
 console.log("everything you need to generate your day trip!");
 
 
-
-
-
-
 let destination = ["milwaukee", "madison", "dells", "devilslake"];
+
+let resturant = ["mcdonalds","dennys","fineDining","flemmin"];
+
+let transportation = ["uber","taxi","rental","train","airplane"];
+
+let entertainment = ["lasertag","movie","swimming","horseRiding","arcade"];
+
+
+
+
+
 function random(myArray){
     return myArray[Math.floor(Math.random() * myArray.length)];
     
@@ -21,30 +28,19 @@ let destinationRandom = random(destination);
 console.log("destination " +destinationRandom);
 
 
-
-
-
-
-let resturant = ["mcdonalds","dennys","fineDining","flemmin"];
 let resturantRandom = random(resturant);
 console.log("resturant " + resturantRandom);
 
 
-
-
-
-
-let transportation = ["uber","taxi","rental","train","airplane"];
 let transportationRandom = random(transportation);
 console.log("transport " + transportationRandom);
 
 
-
-
-
-let entertainment = ["lasertag","movie","swimming","horseRiding","arcade"]
 let entertainmentRandom = random(entertainment);
 console.log("entertainment " + entertainmentRandom);
+
+
+
 
 
 
@@ -60,7 +56,10 @@ if (userInput == "yes"){
     }
 }
 else if (userInput == "no"){
-    console.log("have a great time");
+    while (userInput == "no"){
+        console.log("you have chosen to keep your random trip!");
+        userInput = prompt ("are you sure?")
+    }
 }
 else{
     userInput = prompt ("are you sure?");
@@ -70,11 +69,10 @@ console.log("enjoy your trip!");
 
 
 
-
 function reChoose(){
     let user = prompt("what would you like to change?");
     if (user == "destination"){
-         destinationRandom = random(destination);
+        destinationRandom = random(destination);
         return destinationRandom;
     }
     else if( user === "resturant"){
@@ -95,6 +93,8 @@ function reChoose(){
 
 
 
+
+
 alert("you have choosen " + destinationRandom);
 alert("this will be the place to eat at " + resturantRandom);
 alert("the type of transport will be " + transportationRandom);
@@ -106,3 +106,5 @@ console.log ("the type of transport you will take is " + transportationRandom);
 console.log ( "the type of enjoyment you wish for is " + entertainmentRandom);
 
 console.log (" you have choosen to go to " + destinationRandom + " and eat at "+ resturantRandom + " with the transportation of " + transportationRandom + " to go enjoy doing "  + entertainmentRandom);
+
+alert(" you have choosen to go to " + destinationRandom + " and eat at "+ resturantRandom + " with the transportation of " + transportationRandom + " to go enjoy doing "  + entertainmentRandom);
